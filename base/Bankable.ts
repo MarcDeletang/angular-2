8221360e7bea76f045f8362c5126cf25b633d33f
MarcDeletang <marc.deletang@gmail.com>
2017-01-17 23:28:33 +0100

    Update Bankable.ts

--- base/Bankable.ts
+++ base/Bankable.ts
@@ -1,6 +1,6 @@
 export abstract class Bankable {
 	//constructeur de la classe
-	constructor(_id: number, total: number)
+	constructor(private _id: number,protected _total: number)
 	//vérifie le solde actuel, si la valeur est inférieure retourne true sinon false
 	public canPay(value: number): boolean
 	//ajoute la valeur de amount au solde actuel
